8bdf43ed6eacc4c829302658ce19056fb85b9c36
KeitaroHamada <hamataro1029@gmail.com>
2023-07-12 14:00:53 +0900

    あ

---
+++ src/App.js
@@ -0,0 +1,29 @@
+import { useState ,useRef } from "react";
+import TodoList from "./TodoList";
+
+
+function App() {
+  const [todos, setTodos] = useState([
+    {id: 1, name: "Todo1", completed: false }
+  ]);
+  const todoNameRef = useRef();
+  const handleAddTodo = (e) => {
+    //タスクを追加
+    const name=todoNameRef.current.value;
+    setTodos((prevTodos) => {
+      return [...prevTodos, { id: "1", name: name, completed: false }]
+    }) 
+
+  };
+  return (
+    <div>
+      <TodoList todos ={todos}/>
+      <input type="text" ref={todoNameRef}/>
+      <button onClick={handleAddTodo}>タスクを追加</button>
+      <button>タスクの削除</button>
+      <div>残りのタスク:0</div>
+    </div>
+  );
+}
+
+export default App
